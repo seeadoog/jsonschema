dc392ad2fb9816581e028b8ff8a2d4c4d9dccc6a
sjliu7 <1581503100@qq.com>
2021-05-04 15:07:07 +0800

    add newschema func

--- schema.go
+++ schema.go
@@ -8,57 +8,67 @@ import (
 
 type Schema struct {
 	prop Validator
-	i interface{}
+	i    interface{}
 }
 
-func (s *Schema)UnmarshalJSON(b []byte)error{
+func NewSchema(i map[string]interface{}) (*Schema, error) {
+	s := &Schema{}
+	s.i = i
+	p, err := NewProp(i, "$")
+	if err != nil {
+		return nil, err
+	}
+	s.prop = p
+	return s, nil
+}
+
+func (s *Schema) UnmarshalJSON(b []byte) error {
 	var i interface{}
-	if err:=json.Unmarshal(b,&i);err != nil{
+	if err := json.Unmarshal(b, &i); err != nil {
 		return err
 	}
 	s.i = i
-	p ,err := NewProp(i,"$")
-	if err != nil{
+	p, err := NewProp(i, "$")
+	if err != nil {
 		return err
 	}
 	s.prop = p
 	return nil
 }
 
-func (s *Schema)MarshalJSON()(b []byte,err error){
-	data,err:=json.Marshal(s.i)
-	if err != nil{
-		return nil,err
+func (s *Schema) MarshalJSON() (b []byte, err error) {
+	data, err := json.Marshal(s.i)
+	if err != nil {
+		return nil, err
 	}
-	return data,nil
+	return data, nil
 
 }
 
-func (s *Schema)Validate(i interface{})error{
-	c:=&ValidateCtx{}
-	s.prop.Validate(c,i)
-	if len(c.errors) == 0{
+func (s *Schema) Validate(i interface{}) error {
+	c := &ValidateCtx{}
+	s.prop.Validate(c, i)
+	if len(c.errors) == 0 {
 		return nil
 	}
 	return errors.New(errsToString(c.errors))
 }
 
-func (s *Schema)ValidateError(i interface{})[]Error{
-	c:=&ValidateCtx{}
-	s.prop.Validate(c,i)
+func (s *Schema) ValidateError(i interface{}) []Error {
+	c := &ValidateCtx{}
+	s.prop.Validate(c, i)
 	return c.errors
 }
 
-
-func errsToString(errs []Error)string{
-	sb:=strings.Builder{}
-	n:=0
+func errsToString(errs []Error) string {
+	sb := strings.Builder{}
+	n := 0
 	for _, err := range errs {
-		n+= len(err.Path)+ len(err.Info)+5
+		n += len(err.Path) + len(err.Info) + 5
 	}
 	sb.Grow(n)
 	for _, err := range errs {
-		sb.WriteString(appendString("'",err.Path,"' ",err.Info,"; "))
+		sb.WriteString(appendString("'", err.Path, "' ", err.Info, "; "))
 	}
 	return sb.String()
 }
